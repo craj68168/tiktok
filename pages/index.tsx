58acc34f997f2e8675e5670434458e018b7b1778
razz <craj68168@gmail.com>
2022-08-19 11:33:37 +0545

    ongoing sidebar and navbar

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,11 @@ import Head from "next/head";
 import Image from "next/image";
 
 const Home: NextPage = () => {
-  return <div></div>;
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </div>
+  );
 };
 
 export default Home;
