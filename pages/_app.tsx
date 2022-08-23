06b06e93f4f9073a5a1f447a172943d5848c1935
razz <craj68168@gmail.com>
2022-08-23 13:19:51 +0545

    done:zustand config api built up and get and post request

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,9 @@ import type { AppProps } from "next/app";
 import { useEffect, useState } from "react";
 import Navbar from "../components/Navbar";
 import Sidebar from "../components/Sidebar";
-
+import { GoogleOAuthProvider } from '@react-oauth/google';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 const MyApp = ({ Component, pageProps }: AppProps) => {
   const [isSSR, setIsSSR] = useState(true);
 
@@ -14,7 +16,8 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
   if (isSSR) return null;
 
   return (
-    <div>
+    <>
+    <GoogleOAuthProvider clientId={`${process.env.NEXT_PUBLIC_GOOGLE_API_TOKEN}`}>
       <Navbar/>
       <div className="flex gap-6 md:gap-20">
         <div className="h-[92vh] overflow-hidden xl:hover:overflow-auto"><Sidebar/></div>
@@ -22,7 +25,9 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
           <Component {...pageProps} />
         </div>
       </div>
-    </div>
+    </GoogleOAuthProvider>
+    <ToastContainer position="top-right" />
+    </>
   );
 };
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,34 @@
+import axios from "axios";
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
-
-const Home: NextPage = () => {
+import NoResults from "../components/NoResults";
+import VideoCard from "../components/VideoCard";
+import {Video} from "../types"
+interface IProps {
+  videos:Video[]
+}
+const Home = ({videos}:IProps) => {
+  console.log("videso",videos);
+  
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </div>
+    <div className='flex flex-col gap-10 videos h-full'>
+    {videos.length 
+      ? videos?.map((video: Video) => (
+        <VideoCard post={video} isShowingOnHome key={video._id} />
+      )) 
+      : <NoResults text={`No Videos`} />}
+  </div>
   );
-};
+};  
+
+export const getServerSideProps = async()=>{
+  const {data} = await axios.get(`http://localhost:3000/api/post`);
+  return {
+    props:{
+      videos:data
+    }
+  }
+}
 
 export default Home;
